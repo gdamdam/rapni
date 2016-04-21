89a21967a9fa90dbd22d0a7e744df41ebd5b3630
gio <giovanni@archive.org>
2016-04-20 19:34:47 -0700

    a better name for the initialization bash script file

--- psql_init_db.sh
+++ psql_init_db.sh
@@ -2,7 +2,7 @@
 ##
 ## psql_init_db.sh
 ##
-## This shell script helps to create and initialize the postgres 9.5 db
+## This shell script helps to create and initialize a postgres 9.5 db for rapni
 ##
 ## copyright: (c) 2016 Internet Archive.
 ##    author: Giovanni Damiola <gio@archive.org>
@@ -22,8 +22,8 @@ psqluser='rapni'
 psqlpass='rapni'
 psqldb='rapni'
 
-sudo printf "DROP DATABASE IF EXISTS $psqldb;\nDROP ROLE IF EXISTS $psqluser;\n" > rapni.sql
-sudo printf "CREATE USER $psqluser WITH PASSWORD '$psqlpass';\nCREATE DATABASE $psqldb WITH OWNER $psqluser;" >> rapni.sql
-sudo -u postgres psql -f rapni.sql
+sudo printf "DROP DATABASE IF EXISTS $psqldb;\nDROP ROLE IF EXISTS $psqluser;\n" > rapni_initialize_db.sql
+sudo printf "CREATE USER $psqluser WITH PASSWORD '$psqlpass';\nCREATE DATABASE $psqldb WITH OWNER $psqluser;" >> rapni_initialize_db.sql
+sudo -u postgres psql -f rapni_initialize_db.sql
 
 echo 'Done!'
